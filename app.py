eaa756406882f6bb48d2ca7b231b3b69625ba905
Patricio Leonel Brite <brite878@gmail.com>
2025-11-23 10:55:15 -0300

    agregado modalidad de resultado y logo

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 import math
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from datetime import datetime
 import dateutil.relativedelta
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'dev-key-please-change-in-prod')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.png', mimetype='image/png')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
